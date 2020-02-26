637205b942033b56c9fa69ec35792a938be27398
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-26 17:57:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,8 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable 
   
